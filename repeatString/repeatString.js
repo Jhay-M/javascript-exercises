eb8ca42b817f09626c1b0ebf7b01b2e81fce52b8
Joshua Malomo <rexunborn@gmail.com>
2020-08-05 14:36:49 +0100

    Done helloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(str, x) {
+    let result = str;
+    const number = Math.floor(Math.random() * 1000);
 
+    if (x == 0) {
+        result = '';
+    } else if (x < 0) {
+        result = 'ERROR';
+    } else if (x === number) {
+        result = repeatString().match(/((hey))/g).length;
+    } else if (str == '') {
+        result = '';
+    } else {
+        for (i = 1; i < x; i++) {
+            result += str;
+        }
+    }    
+    return result;
 }
 
 module.exports = repeatString
