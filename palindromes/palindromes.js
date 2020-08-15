b8238c0e18127b913a1392a57bfa69cbefabc4cb
Joshua Malomo <rexunborn@gmail.com>
2020-08-15 23:04:31 +0100

    Done palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
 
+    let pick = str
+                .toLowerCase()
+                .split('')
+                .filter(letter => {
+                    return punctuation.indexOf(letter) === -1;
+                })
+                .join('');
+
+    let reverse = str
+                    .toLowerCase()
+                    .split('')
+                    .reverse()
+                    .filter(letter => {
+                        return  punctuation.indexOf(letter) === -1;
+                    })
+                    .join('');
+
+    return (pick == reverse) ? true: false;
 }
 
 module.exports = palindromes
