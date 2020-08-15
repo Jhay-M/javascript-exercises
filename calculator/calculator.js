b0f9937ea4d2b8569baacbf092237a883762da6d
Joshua Malomo <rexunborn@gmail.com>
2020-08-15 13:22:32 +0100

    Done Calculator Exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,49 @@
-function add () {
-	
+function add (x, y) {//Input Variables X & Y.
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (arr) {//Input is an array.
+    let totalSum = 0;
+	if (arr == []) {
+        return 0;
+    }
+    for (i = 0; i < arr.length; i++) {
+        totalSum += arr[i];
+    }
+    return totalSum;
 }
 
-function multiply () {
-	
+function multiply (arr) {
+    let totalMultiply = 1;
+    for (i = 0; i < arr.length; i++) {
+        totalMultiply *= arr[i];
+    };
+    return totalMultiply;
 }
 
-function power() {
-	
+function power(x, y) {
+    let square = x
+	for (i = 1; i < y; i++) {
+        square *= x;
+    };
+    return square;
 }
 
-function factorial() {
-	
+function factorial(num) {
+    let multiply = num;
+    let drop = num;
+	if (num == 0 || num == 1) {
+        return 1;
+    };
+    for (i = 1; i < num; i++) {
+        multiply *= (drop - 1);
+        drop--;
+    };
+    return multiply;
 }
 
 module.exports = {
