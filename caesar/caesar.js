d97b077180e44ee46be7ee225313a9a366a9d805
Joshua Malomo <rexunborn@gmail.com>
2020-08-17 18:23:09 +0100

    Done Caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,30 +1,32 @@
 const caesar = ((str, int) => {
     let alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
-        let alphaBet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    let alphaBet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 
-        return str
-                .split('')
-                .map(item => {
-                    let smallCap = alphabet.indexOf(item);
-                    let bigCap = alphaBet.indexOf(item);
-                    let smallSum = smallCap + int;
-                    let bigSum = bigCap + int;
+    return str
+            .split('')
+            .map(item => {
+                let smallCap = alphabet.indexOf(item);
+                let bigCap = alphaBet.indexOf(item);
+                let smallSum = smallCap + int;
+                let bigSum = bigCap + int;
                 
-                    if (smallCap >= 0) {
-                        if (smallSum > 25) {
-                            return alphabet[smallSum - 26];
-                        } else if (smallSum < 0) {
-                            return alphabet[26 + smallSum];
-                        } else return alphabet[smallSum];
-                    } else if (bigCap >= 0) {
-                        if (bigSum > 25) {
-                            return alphaBet[bigSum - 26];
-                        } else if ( bigSum < 0) {
-                            return alphaBet[26 + bigSum];
-                        } else return alphaBet[bigSum];
-                    } else return item;
-                })
-                .join('');
+                if (smallCap >= 0) {
+                    if (smallSum > 0) {
+                        return alphabet[smallSum % 26];
+                    } else if (smallSum < 0) {
+                        smallSum++;
+                        return alphabet[25 + (smallSum % 26)];
+                    } else return alphabet[smallSum];
+                } else if (bigCap >= 0) {
+                    if (bigSum > 0) {
+                        return alphaBet[bigSum % 26];
+                    } else if ( bigSum < 0) {
+                        bigSum++;
+                        return alphaBet[25 + (bigSum % 26)];
+                    } else return alphaBet[bigSum];
+                } else return item;
+            })
+            .join('');
 });
 
 module.exports = caesar
