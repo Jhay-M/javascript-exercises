1ba63dfd9bf278223280c497a2a98b324b51e950
Joshua Malomo <rexunborn@gmail.com>
2020-08-17 19:45:25 +0100

    Done Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
-}
+const fibonacci = (index => {
+    let indexNum = Number(index);
+    if (indexNum < 0) {
+        return "OOPS";
+    }
+    let arr = [1, 1];
+    for (i = 2; i < indexNum; i++) {
+        let add = arr[i - 1] + arr[i - 2];
+        arr.push(add);
+    };
+    return arr[indexNum - 1];
+});
 
 module.exports = fibonacci
