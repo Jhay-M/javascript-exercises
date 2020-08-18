4a9a6016375781d98608bbb8b6594ea5eb56e0d4
Joshua Malomo <rexunborn@gmail.com>
2020-08-18 18:15:01 +0100

    Done with getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = (books => {
+    const array = books.map(book => {
+        return book.title;
+    })
+    return array;
+});
 
 module.exports = getTheTitles;
